076e41af5643c78a3550a326fdbe25411e045255
Kamil Kokot <kamil@kokot.me>
2018-06-19 11:08:07 +0200

    Add invoice identifier generator

---
+++ spec/UuidInvoiceIdentifierGeneratorSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Sylius\InvoicingPlugin;
+
+use PhpSpec\ObjectBehavior;
+use Ramsey\Uuid\Uuid;
+use Sylius\InvoicingPlugin\InvoiceIdentifierGenerator;
+
+final class UuidInvoiceIdentifierGeneratorSpec extends ObjectBehavior
+{
+    function it_is_an_invoice_identifier_generator(): void
+    {
+        $this->shouldImplement(InvoiceIdentifierGenerator::class);
+    }
+
+    function it_returns_a_string(): void
+    {
+        $this->__invoke()->shouldBeString();
+    }
+
+    function it_returns_a_uuid4(): void
+    {
+        Uuid::fromString($this->__invoke()->getWrappedObject());
+    }
+
+    function it_returns_two_different_strings_on_subsequent_calls(): void
+    {
+        $this->__invoke()->shouldNotReturn($this->__invoke()->getWrappedObject());
+    }
+}
