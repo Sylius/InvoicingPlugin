8342b625e048d62f7c765203d0934a321bc13b5d
Grzegorz Sadowski <sadowskigp@gmail.com>
2018-06-23 14:10:11 +0200

    [Invoice] Update invoice model

--- src/Entity/InvoiceInterface.php
+++ src/Entity/InvoiceInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Sylius\InvoicingPlugin\Entity;
 
+use Doctrine\Common\Collections\Collection;
+
 interface InvoiceInterface
 {
     public function id(): string;
@@ -11,4 +13,14 @@ interface InvoiceInterface
     public function orderNumber(): string;
 
     public function issuedAt(): \DateTimeInterface;
+
+    public function billingData(): BillingDataInterface;
+
+    public function currencyCode(): string;
+
+    public function taxTotal(): int;
+
+    public function total(): int;
+
+    public function lineItems(): Collection;
 }
