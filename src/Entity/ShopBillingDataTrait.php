dc667d4131d1b02b66ab2e7fabb412dd8f56859c
Grzegorz Sadowski <sadowskigp@gmail.com>
2018-07-02 18:16:15 +0200

    Add shop billing data trait

---
+++ src/Entity/ShopBillingDataTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\InvoicingPlugin\Entity;
+
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\OneToOne;
+
+trait ShopBillingDataTrait
+{
+    /**
+     * @OneToOne(targetEntity="Sylius\InvoicingPlugin\Entity\ShopBillingData", cascade={"persist"})
+     * @JoinColumn(name="billing_data_id", referencedColumnName="id")
+     *
+     * @var ShopBillingDataInterface
+     */
+    private $billingData;
+
+    public function getBillingData(): ?ShopBillingDataInterface
+    {
+        return $this->billingData;
+    }
+
+    public function setBillingData(?ShopBillingDataInterface $billingData): void
+    {
+        $this->billingData = $billingData;
+    }
+}
