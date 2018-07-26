d0c9983d6fdb0da391e414432de8854aae1061cb
Mateusz Zalewski <zaleslaw@gmail.com>
2018-07-26 16:13:55 +0200

    React with OrderPlaced event on doctrine event instead of SM callback

--- spec/EventProducer/OrderPlacedProducerSpec.php
+++ spec/EventProducer/OrderPlacedProducerSpec.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace spec\Sylius\InvoicingPlugin\EventProducer;
 
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use PhpSpec\ObjectBehavior;
 use Prooph\ServiceBus\EventBus;
 use Prophecy\Argument;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\OrderCheckoutStates;
 use Sylius\InvoicingPlugin\DateTimeProvider;
 use Sylius\InvoicingPlugin\Event\OrderPlaced;
 
@@ -18,15 +20,19 @@ final class OrderPlacedProducerSpec extends ObjectBehavior
         $this->beConstructedWith($eventBus, $dateTimeProvider);
     }
 
-    function it_dispatches_an_order_placed_event_for_an_order(
+    function it_dispatches_an_order_placed_event_for_persited_order(
         EventBus $eventBus,
         DateTimeProvider $dateTimeProvider,
+        LifecycleEventArgs $event,
         OrderInterface $order,
         \DateTime $dateTime
     ): void {
         $dateTimeProvider->__invoke()->willReturn($dateTime);
 
+        $event->getEntity()->willReturn($order);
+
         $order->getNumber()->willReturn('000666');
+        $order->getCheckoutState()->willReturn(OrderCheckoutStates::STATE_COMPLETED);
 
         $eventBus
             ->dispatch(Argument::that(function (OrderPlaced $event) use ($dateTime): bool {
@@ -38,6 +44,58 @@ final class OrderPlacedProducerSpec extends ObjectBehavior
             ->shouldBeCalled()
         ;
 
-        $this($order);
+        $this->postPersist($event);
+    }
+
+    function it_dispatches_an_order_placed_event_for_updated_order(
+        EventBus $eventBus,
+        DateTimeProvider $dateTimeProvider,
+        LifecycleEventArgs $event,
+        OrderInterface $order,
+        \DateTime $dateTime
+    ): void {
+        $dateTimeProvider->__invoke()->willReturn($dateTime);
+
+        $event->getEntity()->willReturn($order);
+
+        $order->getNumber()->willReturn('000666');
+        $order->getCheckoutState()->willReturn(OrderCheckoutStates::STATE_COMPLETED);
+
+        $eventBus
+            ->dispatch(Argument::that(function (OrderPlaced $event) use ($dateTime): bool {
+                return
+                    $event->orderNumber() === '000666' &&
+                    $event->date() === $dateTime->getWrappedObject()
+                ;
+            }))
+            ->shouldBeCalled()
+        ;
+
+        $this->postUpdate($event);
+    }
+
+    function it_does_nothing_if_event_entity_is_not_order(EventBus $eventBus, LifecycleEventArgs $event): void
+    {
+        $event->getEntity()->willReturn('notAnOrder', 'notAnOrder');
+
+        $eventBus->dispatch(Argument::any())->shouldNotBeCalled();
+
+        $this->postPersist($event);
+        $this->postUpdate($event);
+    }
+
+    function it_does_nothing_if_order_is_not_completed(
+        EventBus $eventBus,
+        LifecycleEventArgs $event,
+        OrderInterface $order
+    ): void {
+        $event->getEntity()->willReturn($order, $order);
+
+        $order->getCheckoutState()->willReturn(OrderCheckoutStates::STATE_CART, OrderCheckoutStates::STATE_ADDRESSED);
+
+        $eventBus->dispatch(Argument::any())->shouldNotBeCalled();
+
+        $this->postPersist($event);
+        $this->postUpdate($event);
     }
 }
--- src/EventProducer/OrderPlacedProducer.php
+++ src/EventProducer/OrderPlacedProducer.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Sylius\InvoicingPlugin\EventProducer;
 
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Prooph\ServiceBus\EventBus;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\OrderCheckoutStates;
 use Sylius\InvoicingPlugin\DateTimeProvider;
 use Sylius\InvoicingPlugin\Event\OrderPlaced;
 
@@ -23,8 +25,27 @@ final class OrderPlacedProducer
         $this->dateTimeProvider = $dateTimeProvider;
     }
 
-    public function __invoke(OrderInterface $order): void
+    public function postPersist(LifecycleEventArgs $event): void
     {
+        $this->dispatchOrderPlacedEventIfNecessary($event);
+    }
+
+    public function postUpdate(LifecycleEventArgs $event): void
+    {
+        $this->dispatchOrderPlacedEventIfNecessary($event);
+    }
+
+    private function dispatchOrderPlacedEventIfNecessary(LifecycleEventArgs $event): void
+    {
+        $order = $event->getEntity();
+
+        if (
+            !$order instanceof OrderInterface ||
+            $order->getCheckoutState() !== OrderCheckoutStates::STATE_COMPLETED
+        ) {
+            return;
+        }
+
         $this->eventBus->dispatch(
             new OrderPlaced($order->getNumber(), $this->dateTimeProvider->__invoke())
         );
