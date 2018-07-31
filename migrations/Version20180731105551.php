d47d8738d81c0647b688eded0767ef2c04ebec38
Grzegorz Sadowski <sadowskigp@gmail.com>
2018-07-31 12:57:15 +0200

    Add migration file

--- migrations/Version20180731105551.php
+++ migrations/Version20180731105551.php
@@ -10,16 +10,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180711084815 extends AbstractMigration
+final class Version20180731105551 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE sylius_invoicing_plugin_sequence (id INTEGER NOT NULL, idx INTEGER NOT NULL, version INTEGER DEFAULT 1 NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE sylius_invoicing_plugin_invoice ADD number VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE sylius_invoicing_plugin_sequence CHANGE id id INT AUTO_INCREMENT NOT NULL');
+        $this->addSql('ALTER TABLE sylius_invoicing_plugin_sequence ADD last_generated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -27,8 +25,6 @@ final class Version20180711084815 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_invoicing_plugin_invoice DROP number');
-        $this->addSql('ALTER TABLE sylius_invoicing_plugin_sequence CHANGE id id INT NOT NULL');
-        $this->addSql('DROP TABLE sylius_invoicing_plugin_sequence');
+        $this->addSql('ALTER TABLE sylius_invoicing_plugin_sequence DROP last_generated_at');
     }
 }
