db4ace06f736f5e01ec636cb37d753a4f3909ab6
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-08-30 09:49:59 +0200

    Migration namespace configuration + existing namespace fix

--- migrations/Version20180826064735.php
+++ migrations/Version20180826064735.php
@@ -1,6 +1,8 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace Application\Migrations;
+declare(strict_types=1);
+
+namespace Sylius\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
