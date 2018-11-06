835299a4afc8c7ad20422fc4cf0a8c6407c7764f
Francis Hilaire <Prometee@users.noreply.github.com>
2018-11-06 12:52:26 +0100

    Delete nnecessary Channel::tax_id field

--- src/Entity/ShopBillingDataTrait.php
+++ src/Entity/ShopBillingDataTrait.php
@@ -11,29 +11,12 @@ use Doctrine\ORM\Mapping\OneToOne;
 trait ShopBillingDataTrait
 {
     /**
-     * @Column(type="string", nullable=true)
-     *
-     * @var string
-     */
-    private $taxId;
-
-    /**
      * @OneToOne(targetEntity="Sylius\InvoicingPlugin\Entity\ShopBillingData", cascade={"persist"})
      * @JoinColumn(name="billing_data_id", referencedColumnName="id")
      *
      * @var ShopBillingDataInterface
      */
     private $billingData;
-
-    public function getTaxId(): ?string
-    {
-        return $this->taxId;
-    }
-
-    public function setTaxId(?string $taxId): void
-    {
-        $this->taxId = $taxId;
-    }
 
     public function getBillingData(): ?ShopBillingDataInterface
     {
