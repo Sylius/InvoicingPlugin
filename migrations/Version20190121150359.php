9d87f18fa89bf720bc2b285ee6fb9ea011509ca0
Steeve Chopart <schopart@clicher.fr>
2019-01-21 16:08:59 +0100

    add migration file for line item

---
+++ migrations/Version20190121150359.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Sylius\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190121150359 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_invoicing_plugin_line_item ADD type VARCHAR(255) NOT NULL, ADD promotion_total INT NOT NULL, ADD variant_options LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', ADD item_number VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_invoicing_plugin_line_item DROP type, DROP promotion_total, DROP variant_options, DROP item_number');
+    }
+}
