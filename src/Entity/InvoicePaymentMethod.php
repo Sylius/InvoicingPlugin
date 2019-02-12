e30092dbcca642296b6f5447b93036779f8ffb13
Steeve Chopart <schopart@clicher.fr>
2019-02-12 11:08:16 +0100

    add payment method code

--- src/Entity/InvoicePaymentMethod.php
+++ src/Entity/InvoicePaymentMethod.php
@@ -7,10 +7,23 @@ namespace Sylius\InvoicingPlugin\Entity;
 class InvoicePaymentMethod implements InvoicePaymentMethodInterface
 {
     /** @var string|null */
+    protected $code;
+
+    /** @var string|null */
     protected $name;
 
     /** @var string|null */
     protected $instructions;
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): void
+    {
+        $this->code = $code;
+    }
 
     public function getName(): ?string
     {
--- src/Entity/InvoicePaymentMethodInterface.php
+++ src/Entity/InvoicePaymentMethodInterface.php
@@ -6,6 +6,10 @@ namespace Sylius\InvoicingPlugin\Entity;
 
 interface InvoicePaymentMethodInterface
 {
+    public function getCode(): ?string;
+
+    public function setCode(?string $code): void;
+
     public function getName(): ?string;
 
     public function setName(?string $name): void;
