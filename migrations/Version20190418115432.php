0370d39cefe65e8af6d6a723a8bb05141e643be4
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2019-04-18 14:53:24 +0200

    Review fixes

--- migrations/Version20190418115432.php
+++ migrations/Version20190418115432.php
@@ -12,7 +12,7 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190418115432 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -20,7 +20,7 @@ final class Version20190418115432 extends AbstractMigration
         $this->addSql('DROP TABLE sylius_invoicing_plugin_shop_billing_data');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
