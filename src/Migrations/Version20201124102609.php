898b4bacd1b20379b133e60c9bc54184371b8901
Francis Hilaire <Prometee@users.noreply.github.com>
2020-11-24 12:17:53 +0100

    Fix ecs

--- src/Migrations/Version20201124102609.php
+++ src/Migrations/Version20201124102609.php
@@ -9,13 +9,13 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20201124102609 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_invoice CHANGE billing_data_id billing_data_id INT NOT NULL, CHANGE shop_billing_data_id shop_billing_data_id INT NOT NULL, CHANGE channel_id channel_id INT NOT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_invoice CHANGE billing_data_id billing_data_id INT DEFAULT NULL, CHANGE shop_billing_data_id shop_billing_data_id INT DEFAULT NULL, CHANGE channel_id channel_id INT DEFAULT NULL');
