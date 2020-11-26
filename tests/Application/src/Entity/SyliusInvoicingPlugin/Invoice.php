971c3b85fbb4fcff5c03d2122f44ce359d030086
Francis Hilaire <Prometee@users.noreply.github.com>
2020-11-26 19:07:02 +0100

    Simply extends with an upper entity

---
+++ tests/Application/src/Entity/SyliusInvoicingPlugin/Invoice.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\InvoicingPlugin\Application\Entity\SyliusInvoicingPlugin;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\InvoicingPlugin\Entity\Invoice as BaseInvoice;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="sylius_invoicing_plugin_invoice")
+ */
+class Invoice extends BaseInvoice implements InvoiceInterface
+{
+
+}
