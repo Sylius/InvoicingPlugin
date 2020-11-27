7f42774024a00acd8bd79c4edbc84bc97bc6dd5f
Francis Hilaire <Prometee@users.noreply.github.com>
2020-11-27 19:49:14 +0100

    Add all possible entity overrides of this plugin

---
+++ tests/Application/src/Entity/SyliusInvoicingPlugin/InvoiceShopBillingData.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\InvoicingPlugin\Application\Entity\SyliusInvoicingPlugin;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\InvoicingPlugin\Entity\InvoiceShopBillingData as BaseInvoiceShopBillingData;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="sylius_invoicing_plugin_shop_billing_data")
+ */
+class InvoiceShopBillingData extends BaseInvoiceShopBillingData implements InvoiceShopBillingDataInterface
+{
+
+}
---
+++ tests/Application/src/Entity/SyliusInvoicingPlugin/LineItemInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\InvoicingPlugin\Application\Entity\SyliusInvoicingPlugin;
+
+use Sylius\InvoicingPlugin\Entity\LineItemInterface as BaseLineItemInterface;
+
+interface LineItemInterface extends BaseLineItemInterface
+{
+}
