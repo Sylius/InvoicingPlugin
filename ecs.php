400ceda02b1b67542ee6e45cc63fdbbe3a2a85bb
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-06-02 11:09:40 +0200

    Add missing license docblocks

--- ecs.php
+++ ecs.php
@@ -1,11 +1,23 @@
 <?php
 
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void
 {
     $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+
+    $containerConfigurator->services()->set(HeaderCommentFixer::class)->call('configure', [[
+        'location' => 'after_open',
+        'header' =>
+'This file is part of the Sylius package.
+
+(c) Paweł Jędrzejewski
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.',
+    ]]);
 
     $containerConfigurator->parameters()->set(Option::SKIP, [
         '**/var/*',
