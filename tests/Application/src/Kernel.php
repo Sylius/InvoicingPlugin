5f9bd1a124711dcbf6d07617dd9df4a2e21275ad
TheMilek <kam.grygierzec@gmail.com>
2021-12-22 15:02:38 +0100

    Add support for Sylius 1.11-alpha

--- tests/Application/src/Kernel.php
+++ tests/Application/src/Kernel.php
@@ -71,7 +71,7 @@ final class Kernel extends BaseKernel
 
     protected function getContainerBaseClass(): string
     {
-        if ($this->isTestEnvironment()) {
+        if ($this->isTestEnvironment() && class_exists(MockerContainer::class)) {
             return MockerContainer::class;
         }
         return parent::getContainerBaseClass();
