85411511f148def7204131e5d9c009db947c86d5
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2022-10-05 13:03:58 +0200

    Replace gulp with webpack
    Co-authored-by: Jakub Tobiasz <jakub.tobiasz@icloud.com>

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -57,4 +57,5 @@ return [
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
