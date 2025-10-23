bdc9d0dd5ecf48a58f7abcee561e996416970100
Tomasz Kaliński <tomkalon@gmail.com>
2025-10-23 15:11:45 +0200

    Update PHPUnit tests

--- tests/Unit/Generator/InvoiceFileNameGeneratorTest.php
+++ tests/Unit/Generator/InvoiceFileNameGeneratorTest.php
@@ -1,21 +1,11 @@
 <?php
 
-/*
- * This file is part of the Sylius package.
- *
- * (c) Sylius Sp. z o.o.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 namespace Tests\Sylius\InvoicingPlugin\Unit\Generator;
 
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
-use Sylius\InvoicingPlugin\Entity\InvoiceInterface;
 use Sylius\InvoicingPlugin\Generator\InvoiceFileNameGenerator;
 use Sylius\InvoicingPlugin\Generator\InvoiceFileNameGeneratorInterface;
 
@@ -26,6 +16,7 @@ final class InvoiceFileNameGeneratorTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+
         $this->generator = new InvoiceFileNameGenerator();
     }
 
@@ -38,10 +29,27 @@ final class InvoiceFileNameGeneratorTest extends TestCase
     #[Test]
     public function it_generates_invoice_file_name_based_on_its_number(): void
     {
-        $invoice = $this->createMock(InvoiceInterface::class);
-        $invoice->method('number')->willReturn('2020/01/02/000333');
+        $result = $this->generator->generateForPdf('2020/01/02/000333');
 
-        $result = $this->generator->generateForPdf($invoice);
+        self::assertSame('2020_01_02_000333.pdf', $result);
+    }
+
+    #[Test]
+    public function it_generates_scoped_file_name_when_scope_is_set(): void
+    {
+        $generator = new InvoiceFileNameGenerator('monthly');
+
+        $result = $generator->generateForPdf('2020/01/02/000333');
+
+        self::assertSame('monthly/2020_01_02_000333.pdf', $result);
+    }
+
+    #[Test]
+    public function it_uses_global_scope_when_scope_is_invalid_or_null(): void
+    {
+        $generator = new InvoiceFileNameGenerator('invalid_scope');
+
+        $result = $generator->generateForPdf('2020/01/02/000333');
 
         self::assertSame('2020_01_02_000333.pdf', $result);
     }
--- tests/Unit/Provider/InvoiceFileProviderTest.php
+++ tests/Unit/Provider/InvoiceFileProviderTest.php
@@ -31,8 +31,6 @@ use Sylius\PdfGenerationBundle\Core\Model\PdfFile;
 
 final class InvoiceFileProviderTest extends TestCase
 {
-    private InvoiceFileNameGeneratorInterface&MockObject $invoiceFileNameGenerator;
-
     private FilesystemInterface&MockObject $filesystem;
 
     private InvoicePdfFileGeneratorInterface&MockObject $invoicePdfFileGenerator;
@@ -44,13 +42,11 @@ final class InvoiceFileProviderTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->invoiceFileNameGenerator = $this->createMock(InvoiceFileNameGeneratorInterface::class);
         $this->filesystem = $this->createMock(FilesystemInterface::class);
         $this->invoicePdfFileGenerator = $this->createMock(InvoicePdfFileGeneratorInterface::class);
         $this->invoiceFileManager = $this->createMock(InvoiceFileManagerInterface::class);
 
         $this->provider = new InvoiceFileProvider(
-            $this->invoiceFileNameGenerator,
             $this->filesystem,
             $this->invoicePdfFileGenerator,
             $this->invoiceFileManager,
@@ -70,10 +66,9 @@ final class InvoiceFileProviderTest extends TestCase
         $invoice = $this->createMock(InvoiceInterface::class);
         $invoiceFile = $this->createMock(File::class);
 
-        $this->invoiceFileNameGenerator
+        $invoice
             ->expects(self::once())
-            ->method('generateForPdf')
-            ->with($invoice)
+            ->method('path')
             ->willReturn('invoice.pdf');
 
         $this->filesystem
@@ -100,10 +95,9 @@ final class InvoiceFileProviderTest extends TestCase
     {
         $invoice = $this->createMock(InvoiceInterface::class);
 
-        $this->invoiceFileNameGenerator
+        $invoice
             ->expects(self::once())
-            ->method('generateForPdf')
-            ->with($invoice)
+            ->method('path')
             ->willReturn('invoice.pdf');
 
         $this->filesystem
