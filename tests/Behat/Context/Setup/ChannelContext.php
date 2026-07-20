df38698bc23c540593ef9572635c043195ee2c01
Tomasz Kaliński <tomasz.kalinski@bitbag.pl>
2026-07-20 11:00:58 +0200

    ECS fixes

--- tests/Behat/Context/Setup/ChannelContext.php
+++ tests/Behat/Context/Setup/ChannelContext.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Tests\Sylius\InvoicingPlugin\Behat\Context\Setup;
@@ -30,7 +39,7 @@ final class ChannelContext implements Context
         string $street,
         string $postcode,
         string $city,
-        CountryInterface $country
+        CountryInterface $country,
     ): void {
         $shopBillingData = new ShopBillingData();
         $shopBillingData->setCity($city);
--- tests/Behat/Page/Admin/Invoice/IndexPage.php
+++ tests/Behat/Page/Admin/Invoice/IndexPage.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Tests\Sylius\InvoicingPlugin\Behat\Page\Admin\Invoice;
@@ -15,7 +24,7 @@ final class IndexPage extends BaseIndexPage implements IndexPageInterface
 
     public function hasInvoiceWithChannel(int $index, string $channel): bool
     {
-        $invoice = $this->getDocument()->findAll('css', 'table tbody tr')[$index-1];
+        $invoice = $this->getDocument()->findAll('css', 'table tbody tr')[$index - 1];
 
         return $invoice->find('css', sprintf('td:contains("%s")', $channel)) !== null;
     }
